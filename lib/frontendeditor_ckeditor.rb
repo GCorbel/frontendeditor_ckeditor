e38c20d733656c4589c891286a89e3ca0f01fec8
gcorbel <guirec.corbel@gmail.com>
2012-11-19 19:59:39 -0500

    make it works

--- lib/frontendeditor_ckeditor.rb
+++ lib/frontendeditor_ckeditor.rb
@@ -1,5 +1,2 @@
 require "frontendeditor_ckeditor/version"
-
-module FrontendeditorCkeditor
-  # Your code goes here...
-end
+require "frontendeditor_ckeditor/engine"
---
+++ lib/frontendeditor_ckeditor/engine.rb
@@ -0,0 +1,8 @@
+module FrontendeditorCkeditor
+  if defined?(::Rails) and ::Rails.version >= "3.1"
+    module Rails
+      class Engine < ::Rails::Engine
+      end
+    end
+  end
+end
